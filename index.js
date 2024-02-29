6641ce78c876c1d6a9f17dfe14dab664925706a7
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-02-29 04:10:09 -0600

    hjh

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const { Server } = require("socket.io");
-const cors = require("cors");
+
+const cors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  next();
+}
 
 const app = express();
 
@@ -8,7 +15,7 @@ const port = 3005;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors);
 
 const server = app.listen(port, () => {
   console.log(`API corriendo en el puerto ${port}`);
